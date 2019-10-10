220fdde8e1743b46cfefe862ece4480dfb5a03e7
rohitchawla28 <rohitchawla77@gmail.com>
2019-10-10 16:13:32 -0500

    start csv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Data.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Data.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Data {
+
+    public String getDirectory() {
+        return new File("").getAbsolutePath();
+    }
+
+    public void createCSV(String name) throws IOException {
+        PrintWriter writer = new PrintWriter(getDirectory());
+        writ
+    }
 }
