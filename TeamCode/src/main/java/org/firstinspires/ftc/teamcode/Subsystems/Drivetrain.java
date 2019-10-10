48dc07fd0b0ad3d159c4a17c65db7cf179f4033c
rohitchawla28 <rohitchawla77@gmail.com>
2019-10-10 11:31:55 -0500

    drivetrain completed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Drivetrain {
+
+    private LinearOpMode opMode;
+    private Sensors sensors;
+
+    private DcMotor fl, fr, bl, br;
+
+    private final double ticksRev = 383.6;
+    private final double ratio = 1.0;
+    private final double diam = 4.0;
+    private final double cpi = (ticksRev * ratio) / (diam * Math.PI);
+
+    public Drivetrain(LinearOpMode opMode) throws InterruptedException{
+        this.opMode = opMode;
+
+        fl = this.opMode.hardwareMap.dcMotor.get("fl");
+        fr = this.opMode.hardwareMap.dcMotor.get("fr");
+        bl = this.opMode.hardwareMap.dcMotor.get("bl");
+        br = this.opMode.hardwareMap.dcMotor.get("br");
+
+        sensors = new Sensors(opMode);
+
+        fl.setDirection(DcMotor.Direction.REVERSE);
+        fr.setDirection(DcMotor.Direction.FORWARD);
+        bl.setDirection(DcMotor.Direction.REVERSE);
+        br.setDirection(DcMotor.Direction.FORWARD);
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+
+    // ==================================  UTILITY  =============================================
+    public void setPower(double power) {
+        fl.setPower(power);
+        fr.setPower(power);
+        bl.setPower(power);
+        br.setPower(power);
+    }
+
+    public void stopMotors() {
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+    }
+
+    public void resetEncoders() {
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+
+    }
+
+
+    // ==========================================  ENCODER  ========================================
+    public double getLeftInches() {
+        int zeros = 0;
+
+        if (fl.getCurrentPosition() == 0) {
+            zeros++;
+        }
+        if (bl.getCurrentPosition() == 0) {
+            zeros++;
+        }
+
+        return ((fl.getCurrentPosition() / cpi) + (bl.getCurrentPosition() / cpi)) / (2.0 - zeros);
+    }
+
+    public double getRightInches() {
+        int zeros = 0;
+
+        if (fr.getCurrentPosition() == 0) {
+            zeros++;
+        }
+        if (br.getCurrentPosition() == 0) {
+            zeros++;
+        }
+
+        return ((fr.getCurrentPosition() / cpi) + (br.getCurrentPosition() / cpi)) / (2.0 - zeros);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Sensors.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class Sensors {
+    private LinearOpMode opMode;
+
+    public BNO055IMU gyro;
+    private Orientation angles;
+
+    public Sensors(LinearOpMode opMode) throws InterruptedException {
+        this.opMode = opMode;
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        gyro = this.opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        gyro.initialize(parameters);
+
+    }
+
+    public void updateGyroValues() {
+        angles = gyro.getAngularOrientation();
+
+    }
+
+    public double getGyroYaw() {
+        updateGyroValues();
+        return angles.firstAngle;
+
+    }
+
+    public double getGyroPitch() {
+        updateGyroValues();
+        return angles.secondAngle;
+
+    }
+
+    public double getGyroRoll() {
+        updateGyroValues();
+        return angles.thirdAngle;
+
+    }
+
+}
